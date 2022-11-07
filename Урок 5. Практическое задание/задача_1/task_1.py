373ea46331ca34ab6eaf8e893f50e6560aeed194
MegaGnome <111235046+MegaGnome@users.noreply.github.com>
2022-11-07 16:42:02 +0300

    Update and rename Урок 5. Практическое задание/task_1/task_1.py to Урок 5. Практическое задание/задача_1/task_1.py

---
+++ Урок 5. Практическое задание/задача_1/task_1.py
@@ -0,0 +1,19 @@
+"""
+1)	Создать программно файл в текстовом формате,
+записать в него построчно данные, вводимые пользователем.
+Об окончании ввода данных свидетельствует пустая строка.
+"""
+print("Вводите построчные данные.")
+try:
+    with open("user_text.txt", "w+", encoding="utf-8") as f_obj:
+        while True:
+            line = input (">")
+            "" == == "":
+                break
+            f_obj.write(f"{line}\n")
+
+        print("Текущий файл:")
+        f_obj.seek(0)
+        print(f_obj.read())
+except IOError:
+    print("Ошибка!")
